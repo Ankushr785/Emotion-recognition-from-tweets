946d5c43884d817dba0b8b922638e3276fd9f0cd
Ankush Raut <ankushr785@gmail.com>
2017-08-30 07:29:08 +0530

    Add files via upload

---
+++ emotion_recognizer_nbc.py
@@ -0,0 +1,86 @@
+#important libraries
+import pandas as pd
+import numpy as np
+import nltk
+import re
+#importing stopwords is optional, in this case it decreased accuracy
+#from nltk.corpus import stopwords
+import itertools
+
+
+import os
+os.chdir('/home/Akai/Downloads')
+
+
+data = pd.read_csv('text_emotion.csv')
+#data = data.iloc[:5,:]
+
+#stopset = set(stopwords.words('english'))
+
+from nltk.stem.wordnet import WordNetLemmatizer 
+lem = WordNetLemmatizer()
+
+#removal of urls
+for i in range(len(data)):
+    data.content[i] = re.sub(r"http\S+", "", data.content[i])
+
+#removal of html phrases
+for i in range(len(data)):
+    data.content[i] = data.content[i].split()
+    index = 0
+    for j in range(len(data.content[i])):
+        if data.content[i][j][0] == '@':
+            index = j
+    data.content[i] = np.delete(data.content[i], index)
+    words = data.content[i][0]
+    for k in range(len(data.content[i])-1):
+        words+= " " + data.content[i][k+1]
+    data.content[i] = words
+    
+#comprehensive cleaning           
+
+for i in range(len(data)):
+    data.content[i] = re.sub(r'[^\w]', ' ', data.content[i])
+    data.content[i] = ''.join(''.join(s)[:2] for _, s in itertools.groupby(data.content[i]))
+    data.content[i] = data.content[i].replace("'", "")
+    data.content[i] = nltk.tokenize.word_tokenize(data.content[i])
+    #data.content[i] = [w for w in data.content[i] if not w in stopset]
+    for j in range(len(data.content[i])):
+        data.content[i][j] = lem.lemmatize(data.content[i][j], "v")
+    if len(data.content[i]) == 0:
+        data = data.drop(data.index[[i]])
+
+        
+data = data.reset_index(drop=True)
+for i in range(len(data)):
+    words = data.content[i][0]
+    for j in range(len(data.content[i])-1):
+        words+= ' ' + data.content[i][j+1]
+    data.content[i] = words
+    
+x = int(np.round(len(data)*0.75))
+train = data.iloc[:x,:].reset_index(drop = True)
+test = data.iloc[x:,:].reset_index(drop = True)
+    
+from textblob.classifiers import NaiveBayesClassifier as NBC
+
+training_corpus = []
+
+for k in range(len(train)):
+    training_corpus.append((train.content[k], train.sentiment[k]))
+    
+test_corpus = []
+
+for l in range(len(test)):
+    test_corpus.append((test.content[l], test.sentiment[l]))
+
+model = NBC(training_corpus)
+
+print(model.accuracy(test_corpus))
+
+predictions = []
+for m in range(len(test)):
+    predictions.append(model.classify(test.content[m]))
+    
+predictions_df = pd.DataFrame({'Content':test.content, 'Emotion_predicted':predictions, 'Emotion_actual':test.sentiment})
+predictions_df.to_csv('naive_emotion_recognizer.csv', index = False)
