0cddd24109b4def5d32b5ff52cd09143917f5e25
Ankush Raut <ankushr785@gmail.com>
2017-09-03 23:07:53 +0530

    Update emotion_recognizer_nbc.py

--- emotion_recognizer_nbc.py
+++ emotion_recognizer_nbc.py
@@ -6,49 +6,54 @@ import re
 #importing stopwords is optional, in this case it decreased accuracy
 #from nltk.corpus import stopwords
 import itertools
+import json
+import time
+import datetime
 
+
+start_time = time.time()
 
 import os
-os.chdir('/home/Akai/Downloads')
+os.chdir('/home/ankushraut/Downloads')
 
 
 data = pd.read_csv('text_emotion.csv')
-#data = data.iloc[:5,:]
+#data = data.iloc[:100,:]
+
 
 #stopset = set(stopwords.words('english'))
 
 from nltk.stem.wordnet import WordNetLemmatizer 
 lem = WordNetLemmatizer()
 
-#removal of urls
+#comprehensive cleaning
 for i in range(len(data)):
     data.content[i] = re.sub(r"http\S+", "", data.content[i])
-
-#removal of html phrases
-for i in range(len(data)):
-    data.content[i] = data.content[i].split()
-    index = 0
-    for j in range(len(data.content[i])):
-        if data.content[i][j][0] == '@':
-            index = j
-    data.content[i] = np.delete(data.content[i], index)
-    words = data.content[i][0]
-    for k in range(len(data.content[i])-1):
-        words+= " " + data.content[i][k+1]
-    data.content[i] = words
-    
-#comprehensive cleaning           
-
-for i in range(len(data)):
-    data.content[i] = re.sub(r'[^\w]', ' ', data.content[i])
-    data.content[i] = ''.join(''.join(s)[:2] for _, s in itertools.groupby(data.content[i]))
-    data.content[i] = data.content[i].replace("'", "")
-    data.content[i] = nltk.tokenize.word_tokenize(data.content[i])
-    #data.content[i] = [w for w in data.content[i] if not w in stopset]
-    for j in range(len(data.content[i])):
-        data.content[i][j] = lem.lemmatize(data.content[i][j], "v")
     if len(data.content[i]) == 0:
-        data = data.drop(data.index[[i]])
+        data.drop(i, inplace = True)
+    else:
+        data.content[i] = data.content[i].split()
+        index = 0
+        for j in range(len(data.content[i])):
+            if data.content[i][j][0] == '@':
+                index = j
+        data.content[i] = np.delete(data.content[i], index)
+        words = data.content[i][0]
+        for k in range(len(data.content[i])-1):
+            words+= " " + data.content[i][k+1]
+        data.content[i] = words
+        data.content[i] = re.sub(r'[^\w]', ' ', data.content[i])
+        if len(data.content[i]) == 0:
+            data.drop(i, inplace = True)
+        else:
+            data.content[i] = ''.join(''.join(s)[:2] for _, s in itertools.groupby(data.content[i]))
+            data.content[i] = data.content[i].replace("'", "")
+            data.content[i] = nltk.tokenize.word_tokenize(data.content[i])
+            #data.content[i] = [w for w in data.content[i] if not w in stopset]
+            for j in range(len(data.content[i])):
+                data.content[i][j] = lem.lemmatize(data.content[i][j], "v")
+            if len(data.content[i]) == 0:
+                data.drop(i, inplace = True)
 
         
 data = data.reset_index(drop=True)
