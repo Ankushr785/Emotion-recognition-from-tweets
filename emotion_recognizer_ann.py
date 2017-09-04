7b7779d98f03fc445cdd3adb73da2f4ce6dc5af5
Ankush Raut <ankushr785@gmail.com>
2017-09-04 14:22:45 +0530

    Add files via upload

---
+++ emotion_recognizer_ann.py
@@ -0,0 +1,241 @@
+#important libraries
+import pandas as pd
+import numpy as np
+import nltk
+import re
+#importing stopwords is optional, in this case it decreased accuracy
+#from nltk.corpus import stopwords
+import itertools
+import json
+import time
+import datetime
+
+
+start_time = time.time()
+
+import os
+os.chdir('/home/ankushraut/Downloads')
+
+
+data = pd.read_csv('text_emotion.csv')
+#data = data.iloc[:50,:]
+
+
+#stopset = set(stopwords.words('english'))
+
+from nltk.stem.wordnet import WordNetLemmatizer 
+lem = WordNetLemmatizer()
+
+#comprehensive cleaning
+for i in range(len(data)):
+    data.content[i] = re.sub(r"http\S+", "", data.content[i])
+    if len(data.content[i]) == 0:
+        data.drop(i, inplace = True)
+    else:
+        data.content[i] = data.content[i].split()
+        index = 0
+        for j in range(len(data.content[i])):
+            if data.content[i][j][0] == '@':
+                index = j
+        data.content[i] = np.delete(data.content[i], index)
+        if len(data.content[i]) == 0:
+            data.drop(i, inplace = True)
+        else:
+            words = data.content[i][0]
+            for k in range(len(data.content[i])-1):
+                words+= " " + data.content[i][k+1]
+            data.content[i] = words
+            data.content[i] = re.sub(r'[^\w]', ' ', data.content[i])
+            if len(data.content[i]) == 0:
+                data.drop(i, inplace = True)
+            else:
+                data.content[i] = ''.join(''.join(s)[:2] for _, s in itertools.groupby(data.content[i]))
+                data.content[i] = data.content[i].replace("'", "")
+                data.content[i] = nltk.tokenize.word_tokenize(data.content[i])
+                #data.content[i] = [w for w in data.content[i] if not w in stopset]
+                for j in range(len(data.content[i])):
+                    data.content[i][j] = lem.lemmatize(data.content[i][j], "v")
+                if len(data.content[i]) == 0:
+                    data.drop(i, inplace = True)
+
+        
+data = data.reset_index(drop=True)
+for i in range(len(data)):
+    words = data.content[i][0]
+    for j in range(len(data.content[i])-1):
+        words+= ' ' + data.content[i][j+1]
+    data.content[i] = words
+    
+le_train = data.iloc[:int(np.round(len(data)*.75)), :]
+val = data.iloc[int(np.round(len(data)*.75)):,:].reset_index(drop = True)
+    
+training_data = []
+for i in range(len(le_train)):
+    training_data.append({"class": le_train.sentiment[i], "sentence": le_train.content[i]})
+    
+words = []
+classes = []
+documents = []
+
+# loop through each sentence in our training data
+for pattern in training_data:
+    w = nltk.word_tokenize(pattern['sentence'])
+    words.extend(w)
+    documents.append((w, pattern['class']))
+    if pattern['class'] not in classes:
+        classes.append(pattern['class'])
+
+classes = list(set(classes))
+
+# create our training data
+training = []
+output = []
+# create an empty array for our output
+output_empty = [0] * len(classes)
+
+# training set, bag of words for each sentence
+for doc in documents:
+    bag = []
+    pattern_words = doc[0]
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+
+    training.append(bag)
+    # output is a '0' for each tag and '1' for current tag
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+    output.append(output_row)
+
+
+# sigmoid functions
+def sigmoid(x):
+    output = 1/(1+np.exp(-x))
+    return output
+
+# sigmoid ka derivative
+def sigmoid_output_to_derivative(output):
+    return output*(1-output)
+ 
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    return sentence_words
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+def bow(sentence, words):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0]*len(words)  
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s: 
+                bag[i] = 1
+    return(np.array(bag))
+
+def model(sentence):
+    x = bow(sentence.lower(), words)
+    # input layer is our bag of words
+    l0 = x
+    # matrix multiplication of input and hidden layer
+    l1 = sigmoid(np.dot(l0, synapse_0))
+    # output layer
+    l2 = sigmoid(np.dot(l1, synapse_1))
+    return l2
+
+def train(X, y, hidden_neurons=10, alpha=1, epochs=5000, dropout=False, dropout_percent=0.2):
+    np.random.seed(1)
+
+    last_mean_error = 1
+    # randomly initialize our weights with mean 0
+    synapse_0 = 2*np.random.random((len(X[0]), hidden_neurons)) - 1
+    synapse_1 = 2*np.random.random((hidden_neurons, len(classes))) - 1
+
+    prev_synapse_0_weight_update = np.zeros_like(synapse_0)
+    prev_synapse_1_weight_update = np.zeros_like(synapse_1)
+
+    synapse_0_direction_count = np.zeros_like(synapse_0)
+    synapse_1_direction_count = np.zeros_like(synapse_1)
+        
+    for j in iter(range(epochs+1)):
+
+        # Feed forward through layers 0, 1, and 2
+        layer_0 = X
+        layer_1 = sigmoid(np.dot(layer_0, synapse_0))
+                
+        if(dropout):
+            layer_1 *= np.random.binomial([np.ones((len(X),hidden_neurons))],1-dropout_percent)[0] * (1.0/(1-dropout_percent))
+
+        layer_2 = sigmoid(np.dot(layer_1, synapse_1))
+
+        layer_2_error = y - layer_2
+
+        if (j% 1000) == 0 and j > 500:
+            # if this 1k iteration's error is greater than the last iteration, break out
+            if np.mean(np.abs(layer_2_error)) < last_mean_error:
+                print ("delta after "+str(j)+" iterations:" + str(np.mean(np.abs(layer_2_error))) )
+                last_mean_error = np.mean(np.abs(layer_2_error))
+            else:
+                print ("break:", np.mean(np.abs(layer_2_error)), ">", last_mean_error )
+                break
+                
+        layer_2_delta = layer_2_error * sigmoid_output_to_derivative(layer_2)
+
+        layer_1_error = layer_2_delta.dot(synapse_1.T)
+        layer_1_delta = layer_1_error * sigmoid_output_to_derivative(layer_1)
+        
+        synapse_1_weight_update = (layer_1.T.dot(layer_2_delta))
+        synapse_0_weight_update = (layer_0.T.dot(layer_1_delta))
+        
+        if(j > 0):
+            synapse_0_direction_count += np.abs(((synapse_0_weight_update > 0)+0) - ((prev_synapse_0_weight_update > 0) + 0))
+            synapse_1_direction_count += np.abs(((synapse_1_weight_update > 0)+0) - ((prev_synapse_1_weight_update > 0) + 0))        
+        
+        synapse_1 += alpha * synapse_1_weight_update
+        synapse_0 += alpha * synapse_0_weight_update
+        
+        prev_synapse_0_weight_update = synapse_0_weight_update
+        prev_synapse_1_weight_update = synapse_1_weight_update
+
+    now = datetime.datetime.now()
+
+    # persist synapses
+    synapse = {'synapse0': synapse_0.tolist(), 'synapse1': synapse_1.tolist(),
+               'datetime': now.strftime("%Y-%m-%d %H:%M"),
+               'words': words,
+               'classes': classes
+              }
+    synapse_file = "synapses.json"
+    
+X = np.array(training)
+y = np.array(output)
+
+train(X, y, hidden_neurons=20, alpha=0.1, epochs=10000, dropout=False, dropout_percent=0.2)
+
+# load our calculated synapse values
+synapse_file = 'synapses.json' 
+with open(synapse_file) as data_file: 
+    synapse = json.load(data_file) 
+    synapse_0 = np.asarray(synapse['synapse0']) 
+    synapse_1 = np.asarray(synapse['synapse1'])
+
+def classify(sentence):
+    results = model(sentence)
+    index = 0
+    for i in range(len(results)):
+        if results[i] == np.max(results):
+            index = i
+    return classes[index]
+
+#validation
+
+predictions = []
+for i in range(len(val)):
+    predictions.append(classify(val.content[i]))
+
+from sklearn.metrics import f1_score
+score = f1_score(val.sentiment, predictions, average='macro') 
+print("f1-score :", score) 
+
+prediction_df = pd.DataFrame({'content':val.content, 'sentiment_predicted':predictions, 'sentimentActual':val.sentiment})
+
+prediction_df.to_csv('emotion_recognizer.csv', index = False)
+elapsed_time = time.time() - start_time
+print ("processing time:", elapsed_time, "seconds")
